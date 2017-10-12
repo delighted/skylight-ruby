b4b7208cb04f837b6ddeea814649f810f988143d
Mark Dodwell <markdodwell@gmail.com>
2017-10-12 00:06:43 -0700

    bump skylight

--- lib/skylight/probes/tilt.rb
+++ lib/skylight/probes/tilt.rb
@@ -3,6 +3,8 @@ module Skylight
   module Probes
     module Tilt
       class Probe
+        VIRTUAL_PATH_PREFIX_PTN = /^(app|lib|vendor)\/(views|assets)\//
+
         def install
           ::Tilt::Template.class_eval do
             alias render_without_sk render
@@ -10,11 +12,22 @@ module Skylight
             def render(*args, &block)
               opts = {
                 category: "view.render.template",
-                title: options[:sky_virtual_path] || basename || "Unknown template name"
+                title: options[:sky_virtual_path] || infer_sky_virtual_path || "Unknown template name"
               }
 
               Skylight.instrument(opts) do
                 render_without_sk(*args, &block)
+              end
+            end
+
+            private
+
+            def infer_sky_virtual_path
+              if expanded_path = file && File.expand_path(file)
+                Pathname.new(expanded_path)
+                  .relative_path_from(Rails.root)
+                  .to_s
+                  .gsub(Skylight::Probes::Tilt::Probe::VIRTUAL_PATH_PREFIX_PTN, "")
               end
             end
           end
